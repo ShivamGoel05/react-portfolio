742710e9b7cc67f7765874e208c8755cb8680540
Shivam Goel <110183387+ShivamGoel05@users.noreply.github.com>
2026-03-31 22:20:36 +0530

    Update BaseLayout.js

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -50,7 +50,7 @@ export default function BaseLayout() {
                <Box component={'footer'} display={'flex'} flexDirection={'column'} alignItems={'center'}
                   py={'1.5rem'} sx={{ opacity: 0.7 }} width={'100%'}>
                   <p>Developed by Shivam Goel</p>
-                  <p>&copy; 2024</p>
+                  <p>&copy; 2026</p>
                </Box>
             </Grid>
          </Grid>
